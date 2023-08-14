62228df104858f8dde0686c997f0ba9d359a51a8
Lavesh Bhatia <Lavesh.Bhatia@alaskaair.com>
2023-08-14 07:34:56 -0700

    finished-GA-5

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import './styling.css'
+
+export default function Banner() {
+    return(
+    <div>
+    <header>Orbit Report</header>
+    <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+    );
+}
+
